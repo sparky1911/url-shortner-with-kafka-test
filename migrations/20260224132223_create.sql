64c4a5c7d47688b0dcc4eda4f2bc53cdb3d55091
sparky1911 <singhvikramshashank@gmail.com>
2026-02-26 01:06:52 +0530

    init commit url shortner with kafka analytics

--- migrations/20260224132223_create.sql
+++ migrations/20260224132223_create.sql
@@ -1,15 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
-CREATE TABLE urls (
+CREATE TABLE IF NOT EXISTS urls (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     short_code VARCHAR(10) UNIQUE NOT NULL,
     long_url TEXT NOT NULL,
-    -- This column automatically generates a binary hash of the long_url for O(1) lookups
     url_hash BINARY(16) AS (UNHEX(MD5(long_url))) STORED,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    -- Index for the redirect (GET /:code)
     INDEX idx_short_code (short_code),
-    -- Unique Index for deduplication (POST /shorten)
     UNIQUE INDEX idx_url_hash (url_hash)
 ) ENGINE=InnoDB;
 -- +goose StatementEnd
